394b9b54d17b9a756abe6110a36c500aef8a8eb9
Thai Nguyen Hoang <jaskai163@gmail.com>
2026-06-20 22:36:59 +0700

    add statistics

--- sql/mysql/hotel.sql
+++ sql/mysql/hotel.sql
@@ -2,4 +2,4 @@ USE hotel_management;
 GO
 
 INSERT INTO Hotel (HotelName, Address, Postcode, City, Country, NumRooms, PhoneNo) VALUES 
-    ('Grand Palace Hotel', '123 Main St', '10000', 'Hanoi', 'Vietnam', 20, '024-3822-0000');
+    ('Grand Palace Hotel', '123 Ton Duc Thang St', '10000', 'HoChiMinh City', 'Vietnam', 20, '012-345-6789');
